9a077ae694661fd29bca06565a35cffe7c8e8a8e
Jossua Nabec <nabec.j@stjolorient.fr>
2025-01-13 11:57:49 +0100

    modif erreur4 totale diff

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,11 +1,11 @@
 """Module manipulant des fichiers texte."""
 def diff(file_first, file_second):
-	resultat=False
-	with open(file_first) as file1_id:
-		with open(file_second) as file2_id:
-			resultat=file1_id.read()!=file2_id.read()
-	return resultat
+    resultat=False
+    with open(file_first) as file1_id:
+        with open(file_second) as file2_id:
+            resultat=file1_id.read()!=file2_id.read()
+    return resultat
 
 def same(file_first, file_second):
-    """Fonction retournant True si deux fichiers sont identiques."""
+	"""Fonction retournant True si deux fichiers sont identiques."""
     return True
