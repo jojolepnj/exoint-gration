60485a2ae237a83e03f73264c6c4073c1f89e6c2
Jossua Nabec <nabec.j@stjolorient.fr>
2025-01-13 11:59:17 +0100

    modif diff

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -7,5 +7,5 @@ def diff(file_first, file_second):
     return resultat
 
 def same(file_first, file_second):
-	"""Fonction retournant True si deux fichiers sont identiques."""
+    """Fonction retournant True si deux fichiers sont identiques."""
     return True
