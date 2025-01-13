22b6b3aaa71044fa14de5ad4e0bfa6a02da0fbcf
Jossua Nabec <nabec.j@stjolorient.fr>
2025-01-13 11:24:53 +0100

    modif erreur1 totale 2 diff

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -4,7 +4,7 @@ def diff(file_first, file_second):
 	resultat=False
 	with open(file_first) as file1_id:
 		with open(file_second) as file2_id:
-			resultat=file1_id.read()!=file2_id.read()!
+			resultat=file1_id.read()!=file2_id.read()
     return resultat
 
 def same(file_first, file_second):
